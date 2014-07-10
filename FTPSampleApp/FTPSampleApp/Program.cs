e8f2fbd93c1f9fc035f43fe6f01a04f2c6ba0b3c
mobeenfolio3 <mobeen@folio3.com>
2014-07-10 09:34:50 +0500

    Sample App to show how to use

---
+++ FTPSampleApp/FTPSampleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTPSampleApp
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Starting uploader app\r\n\r\n");
+         Console.WriteLine("Initializing Handler\r\n\r\n");
+         FTPHandler.FTPRequest ftp = new FTPHandler.FTPRequest();
+         Console.WriteLine("Starting uploading file");
+         ftp.Upload(System.Environment.CurrentDirectory + "\\YourSoftwareLicence.txt");
+         Console.WriteLine("Upload completed successfully\r\n\r\n");
+
+         Console.WriteLine("Start downloading file");
+         ftp.DownloadFile("YourSoftwareLicence.txt","d:\\");
+         Console.WriteLine("Downlading completed\r\n\r\n");
+
+         Console.WriteLine("Renaming file");
+         ftp.Rename("YourSoftwareLicence.txt", "myfile.txt");
+         Console.WriteLine("Rename completed\r\n\r\n");
+
+         Console.WriteLine("Get Ftp File list");
+         string [] lst =ftp.GetFTPFileList();
+         foreach (var item in lst)
+         {
+            Console.WriteLine(item);
+         }
+         Console.WriteLine("Files listed\r\n\r\n");
+         Console.WriteLine("\r\n");
+         Console.WriteLine("Delete file from the server");
+         ftp.Delete("myfile.txt");
+         Console.WriteLine("Deletion completed\r\n\r\n");
+
+
+         Console.ReadLine();
+        
+      }
+   }
+}
